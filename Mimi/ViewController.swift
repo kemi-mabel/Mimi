99a854f1abd7f92c494c21d77524471083d272f4
Oluwakemi Onajinrin <oluwakemi.mabell@gmail.com>
2022-06-03 12:16:54 +0100

    Initial Commit

---
+++ Mimi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mimi
+//
+//  Created by Oluwakemi Onajinrin on 6/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
